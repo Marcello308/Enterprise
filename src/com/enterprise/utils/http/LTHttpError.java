9e1bed4a157a8a8a1ca727cf227f9907f51fd6dd
marcello <461658542@qq.com>
2013-08-20 23:31:40 +0800

    BaseActivity,BaseLayout

---
+++ src/com/enterprise/utils/http/LTHttpError.java
@@ -0,0 +1,24 @@
+/**
+ * FlieName:LTHttpError.java
+ * Destribution:
+ * Author:michael
+ * 2013-5-20 上午9:47:29
+ */
+package com.enterprise.utils.http;
+
+/**
+ * @author michael
+ *
+ */
+public class LTHttpError {
+	
+	
+	public LTHttpError(){
+		errorMessage = "";
+	}
+	
+	public String errorCode;
+	
+	public String errorMessage;
+	
+}
