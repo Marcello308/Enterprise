b1d4ff367b4c67bc1e839e9dae138aa9c1b14678
marcello <461658542@qq.com>
2013-11-05 22:45:25 +0800

    文章详情页2

---
+++ src/com/enterprise/model/ArticleDetail.java
@@ -0,0 +1,16 @@
+package com.enterprise.model;
+
+public class ArticleDetail {
+		public String title;
+		public String info;
+		public String articleDetail;
+		public ArticleDetail(String title, String info, String articleDetail) {
+			super();
+			this.title = title;
+			this.info = info;
+			this.articleDetail = articleDetail;
+		}
+		public ArticleDetail() {
+			super();
+		}
+}
